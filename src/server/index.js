b29ae5062451b9e9002ea994ce4aa7dd4bd920cb
sebas-ib <ibarraperezsebastian@gmail.com>
2025-03-10 12:23:46 -0700

    Added data storage
    When someone sends a message, the message is saved and can be seen even if page is refreshed

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,17 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./polling-app-882ec-firebase-adminsdk-fbsvc-2b9c4a02c0.json");
+const { Timestamp } = require("firebase-admin/firestore");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -11,7 +22,36 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("A user connected");
-  socket.on("message", (msg) => io.emit("message", msg));
+
+  const fetchMessages = async () => {
+    try {
+      const messagesSnapshot = await db
+        .collection("messages")
+        .orderBy("timestamp")
+        .get();
+      messagesSnapshot.forEach((doc) => {
+        socket.emit("message", doc.data().text);
+      });
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  };
+
+  // Call the async function
+  fetchMessages();
+
+  socket.on("message", async (msg) => {
+    try {
+      await db.collection("messages").add({
+        text: msg,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      });
+      io.emit("message", msg);
+    } catch (error) {
+      console.error("Error saving message:", error);
+    }
+  });
+
   socket.on("disconnect", () => console.log("A user disconnected"));
 });
 
