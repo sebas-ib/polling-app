2728c181ea01653e570cbed78f4e698affc30236
Sebastian Ibarra-Perez <113726572+sebas-ib@users.noreply.github.com>
2025-03-21 11:57:18 -0700

    Update app_setup.py

--- src/server/app_setup.py
+++ src/server/app_setup.py
@@ -1,4 +1,7 @@
 import firebase_admin
+import os
+import base64
+import json
 from firebase_admin import credentials, firestore
 from flask import Flask
 from flask_socketio import SocketIO
@@ -11,6 +14,14 @@ CORS(app, supports_credentials=True)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Initialize Firestore
-cred = credentials.Certificate("/polling-app-882ec-firebase-adminsdk-fbsvc-3c08c94340.json")
+firebase_key_b64 = os.getenv("FIREBASE_ADMIN_KEY_BASE64")
+
+if not firebase_key_b64:
+    raise RuntimeError("Missing FIREBASE_ADMIN_KEY_BASE64 environment variable")
+
+firebase_key_json = base64.b64decode(firebase_key_b64).decode("utf-8")
+cred_dict = json.loads(firebase_key_json)
+cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred)
 db = firestore.client()
