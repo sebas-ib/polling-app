6339f6a3aaa0dfa8d139fbdc4793edf807e090c3
sebas-ib <ibarraperezsebastian@gmail.com>
2025-03-21 13:50:50 -0700

    Private key guide

--- src/server/app_setup.py
+++ src/server/app_setup.py
@@ -11,6 +11,6 @@ CORS(app, supports_credentials=True)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Initialize Firestore
-cred = credentials.Certificate("src/server/polling-app-882ec-firebase-adminsdk-fbsvc-b1eea8065e.json")
+cred = credentials.Certificate("your-private-key-goes-here")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
