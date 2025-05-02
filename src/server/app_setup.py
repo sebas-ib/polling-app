06e14230013e9c0a986d0ed3165a6ead3da58978
sebas-ib <ibarraperezsebastian@gmail.com>
2025-05-02 12:01:28 -0700

    Graphing updates

--- src/server/app_setup.py
+++ src/server/app_setup.py
@@ -5,9 +5,8 @@ from pymongo import MongoClient
 from home import register_app_routes
 
 # Create a socketio instance
-# socketio = SocketIO(cors_allowed_origins="*")
-socketio = SocketIO(cors_allowed_origins=["https://polling-app-git-main-polling-app-project.vercel.app/"])
-
+socketio = SocketIO(cors_allowed_origins="*")
+# socketio = SocketIO(cors_allowed_origins=["https://polling-app-three.vercel.app"])
 
 # Initialize variables for the mongodb client and database
 mongo_client = None
@@ -23,8 +22,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'your_secret_key'
 
     # Enable CORS so frontend apps can make requests to the backend
-    # CORS(app, supports_credentials=True)
-    CORS(app, supports_credentials=True, origins=["https://polling-app-git-main-polling-app-project.vercel.app/"])
+    CORS(app, supports_credentials=True)
+    # CORS(app, supports_credentials=True, origins=["https://polling-app-three.vercel.app"])
+
 
     # Connect to MongoDB running on docker service named mongo, timeout in case db is unresponsive
     global mongo_client, db
