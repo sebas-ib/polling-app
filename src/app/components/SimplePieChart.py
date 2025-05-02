dc1a686df71fa6e940cd1c303a092b796f793738
Ctreziok <ctreziok8175@sdsu.edu>
2025-05-02 12:02:36 -0700

    Added labels and Datapoints

--- src/app/components/SimplePieChart.py
+++ src/app/components/SimplePieChart.py
@@ -1,19 +1,23 @@
+
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const SimplePieChart = () => {
+const SimplePieChart = ({ labels, dataPoints }) => {
   const data = {
-    labels: ['Red', 'Blue', 'Yellow'],
+    labels: labels,
     datasets: [
       {
         label: '# of Votes',
-        data: [12, 19, 3],
+        data: dataPoints,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.7)',
-          'rgba(54, 162, 235, 0.7)',
-          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 99, 132, 0.7)',   // Red
+          'rgba(255, 206, 86, 0.7)',   // Yellow
+          'rgba(54, 162, 235, 0.7)',   // Blue
+          'rgba(75, 192, 192, 0.7)',   // Green
+          'rgba(153, 102, 255, 0.7)',  // Purple
+          'rgba(255, 180, 100, 0.7)',  // Orange
         ],
         borderWidth: 1,
       },
